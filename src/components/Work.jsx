bec6c6b8685e46c0ad204bc6f8f7f283202a55b2
LeonardoConde1 <101907018+LeonardoConde1@users.noreply.github.com>
2022-11-29 22:46:44 -0800

    Update Work.jsx

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -40,7 +40,7 @@ const Work = () => {
             <a target="_blank" href="https://github.com/LeonardoConde1/WheatherApp">
               <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Demo</button>
             </a>
-            <a target="_blank" href='https://leonardoconde1.github.io/WheatherApp/'>
+            <a target="_blank" href='https://github.com/LeonardoConde1/WheatherApp.git'>
               <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Code</button>
             </a>
         </div>
