d38b9682b4284c72901f5feb957a8f29e29eb767
LeonardoConde1 <101907018+LeonardoConde1@users.noreply.github.com>
2022-11-29 22:42:45 -0800

    Update Work.jsx

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -40,7 +40,7 @@ const Work = () => {
             <a href="/">
               <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Demo</button>
             </a>
-            <a target="_blank" href='https://github.com/LeonardoConde1/WeatherApp'>
+            <a target="_blank" href=https://leonardoconde1.github.io/WheatherApp/'>
               <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Code</button>
             </a>
         </div>
